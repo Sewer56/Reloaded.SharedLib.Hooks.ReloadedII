4fc74a4335d4e64c844e61099c021eaf95fdb8ea
Sewer56 <seweratwork@gmail.com>
2019-08-09 14:55:28 +0100

    Initial Publish

---
+++ Publish.ps1
@@ -0,0 +1,27 @@
+# Project Output Paths
+$modOutputPath = "Reloaded.Hooks.ReloadedII/bin"
+$solutionName = "Reloaded.Hooks.ReloadedII.sln"
+$publishName = "reloaded.sharedlib.hooks.zip"
+$publishDirectory = "Publish"
+
+if ([System.IO.Directory]::Exists($publishDirectory)) {
+	Get-ChildItem $publishDirectory -Include * -Recurse | Remove-Item -Force -Recurse
+}
+
+# Build
+dotnet restore $solutionName
+dotnet clean $solutionName
+dotnet build -c Release $solutionName
+
+# Cleanup
+Get-ChildItem $modOutputPath -Include *.pdb -Recurse | Remove-Item -Force -Recurse
+Get-ChildItem $modOutputPath -Include *.xml -Recurse | Remove-Item -Force -Recurse
+
+# Make compressed directory
+if (![System.IO.Directory]::Exists($publishDirectory)) {
+    New-Item $publishDirectory -ItemType Directory
+}
+
+# Compress
+Add-Type -A System.IO.Compression.FileSystem
+[IO.Compression.ZipFile]::CreateFromDirectory( $modOutputPath + '/Release', 'Publish/' + $publishName)
---
+++ Reloaded.Hooks.ReloadedII/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Reloaded.Hooks.Definitions;
+using Reloaded.Hooks.ReloadedII.Interfaces;
+using Reloaded.Mod.Interfaces;
+using Reloaded.Mod.Interfaces.Internal;
+
+namespace Reloaded.Hooks.ReloadedII
+{
+    public class Program : IMod, IExports
+    {
+        private IModLoader _modLoader;
+        private IReloadedHooks _reloadedHooks;
+
+        public void Start(IModLoaderV1 loader)
+        {
+            _modLoader = (IModLoader)loader;
+
+            /* Your mod code starts here. */
+            _reloadedHooks = new ReloadedHooks();
+            _modLoader.AddOrReplaceController<IReloadedHooks>(this, _reloadedHooks);
+        }
+
+        /* Mod loader actions. */
+        public void Suspend() { }
+        public void Resume()  { }
+        public void Unload()  { }
+
+        public bool CanUnload()  => false;
+        public bool CanSuspend() => false;
+
+        /* Automatically called by the mod loader when the mod is about to be unloaded. */
+        public Action Disposing { get; }
+        public Type[] GetTypes() => new[] { typeof(IReloadedHooks), typeof(IHook<>) };
+
+        // Note: The reason we are Sharing IHook is because the other mods
+        // (in their own AssemblyLoadContexts) will also need to load the same instance of the Reloaded.Hooks.Definitions library.
+
+        // Warning: This forces other libraries to use the same version of IHook. Changes to IHook can cause breakages!
+        // It is recommended to follow the same versioning rules as defined in "Inter-Mod-Communication".
+    }
+}
